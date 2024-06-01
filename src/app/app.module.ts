980200ad9e9c4b0b6d9280a914c9e2408bcbf741
Sandeep309 <sandeepsaini6237@gmail.com>
2024-06-01 18:48:28 +0530

    ADD: card-theme directive (background + hover event handle)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,16 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { ThemeModeDirective } from './directives/theme-mode.directive';
+import { CardThemeDirective } from './directives/card-theme.directive';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { FooterMenuComponent } from './components/footer-menu/footer-menu.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteCardComponent,
-    ThemeModeDirective,
+    CardThemeDirective,
     FooterMenuComponent,
   ],
   imports: [
@@ -36,6 +37,10 @@ import { FooterMenuComponent } from './components/footer-menu/footer-menu.compon
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
   ],
   bootstrap: [AppComponent],
 })
